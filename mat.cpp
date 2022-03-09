f49daf64c268b2c86d67663a2b3d6584e1d4b0f1
khsabrina <sabrina.khazanov@gmail.com>
2022-03-09 14:14:19 +0200

    first commit

--- mat.cpp
+++ mat.cpp
@@ -16,45 +16,45 @@ namespace ariel{
             throw invalid_argument{"Invalid input of char"};
         }
         string mat_str;
-        vector<vector<char>> mat_num(row, vector<char>(col));
-        int mat =0;
+        vector<vector<char>> mat(row, vector<char>(col));
+        int mat_num =0;
         if (col<row){
-             mat = col / 2 +1;
+             mat_num = col / 2 +1;
         }
-        else {mat = row /2 +1;}
-        for (int i = 0; i< mat; i++){
+        else {mat_num = row /2 +1;}
+        for (int i = 0; i< mat_num; i++){
             if (i % 2 == 0){
                 for(int j = i; j<col-i;j++){
-                    mat_num[i][j] = a;
+                    mat[i][j] = a;
                 }
                 for(int j = i; j<row-i;j++){
-                    mat_num[j][i] = a;
+                    mat[j][i] = a;
                 }
                 for(int j = i; j<col -i;j++){
-                    mat_num[row-i-1][j] = a;
+                    mat[row-i-1][j] = a;
                 }
                 for(int j = i; j<row-i;j++){
-                    mat_num[j][col-i-1] = a;
+                    mat[j][col-i-1] = a;
                 }
             }
             else{
                 for(int j = i; j<col-i;j++){
-                    mat_num[i][j] = b;
+                    mat[i][j] = b;
                 }
                 for(int j = i; j<row-i;j++){
-                    mat_num[j][i] = b;
+                    mat[j][i] = b;
                 }
                 for(int j = i; j<col-i;j++){
-                    mat_num[row-i-1][j] = b;
+                    mat[row-i-1][j] = b;
                 }
                 for(int j = i; j<row-i;j++){
-                    mat_num[j][col-i-1] = b;
+                    mat[j][col-i-1] = b;
                 }
             }
         }
         for (int i=0; i<row;i++){
             for(int j=0;j<col;j++){
-                mat_str += mat_num[i][j];
+                mat_str += mat[i][j];
             }
             mat_str +=  "\n";
         }
--- mat.hpp
+++ mat.hpp
@@ -2,5 +2,5 @@
 using namespace std;
 
 namespace ariel{
-    string mat(int col, int row, char a, char b);
+    string mat(int , int , char, char);
 };
